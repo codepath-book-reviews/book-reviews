fc5556dea474a9ec5b45637822aee1c5873b0244
Sabrina Chen <chensabrina45@gmail.com>
2022-04-22 17:09:54 -0400

    Revert "Added Login functionality using the Parse database"
    This reverts commit a84378337b255547eae024d5395c9c920734558b.

---
+++ book-review/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  book-review
+//
+//  Created by Danny Dong on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
