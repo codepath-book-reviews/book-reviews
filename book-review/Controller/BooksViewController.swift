c431666371f37aa7f0a588584dff7bb5e98af170
Zhi Dong Dong <zhi.2018.nycms@gmail.com>
2022-04-30 10:07:26 -0400

    added search and autolayout

--- book-review/Controller/BooksViewController.swift
+++ book-review/Controller/BooksViewController.swift
@@ -6,43 +6,36 @@
 //
 
 import UIKit
+import AlamofireImage
 
-
-class BooksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+class BooksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     var books = Books.books
-    var searchTerm = "Harry+Potter"
     @IBOutlet weak var tableView: UITableView!
     
-//    let searchController = UISearchController(searchResultsController: ResultsTableViewController())
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    @IBOutlet weak var userSearchInput: UITextField!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        userSearchInput.delegate = self
-        searchForBooks(searchTerm)
+        searchBar.delegate = self
+        searchForBooks("Harry+Potter")
         
-        
-//        navigationItem.searchController = searchController
-//        searchController.obscuresBackgroundDuringPresentation = true
-//        searchController.searchResultsUpdater = self
-//        searchController.searchBar.placeholder = "Find a book"
-//        searchController.searchBar.delegate = self
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let text = userSearchInput.text {
-            print(text)
-            searchForBooks(cleanString(text))
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.resignFirstResponder()
+        return true
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let input = searchBar.text {
+  
+            searchForBooks(cleanString(input))
         }
     }
 
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books.bookList.count
     }
@@ -55,19 +48,27 @@ class BooksViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
         let title = bookDetails.title
         let author = bookDetails.authors[0]
-
+        let thumbnail = bookDetails.imageLinks.thumbnail
+        
+        let thumbnailURL = URL(string: thumbnail)!
+        
         cell.titleLabel.text = title
         cell.authorLabel.text = author
-
+        cell.coverImage.af.setImage(withURL: thumbnailURL)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        books.chosenBook = books.bookList[indexPath.row]
     }
     
     func parseJSON(_ bookList: Data) -> [BookModel]? {
         let decoder = JSONDecoder()
-        
-        do {
-            let decodedBookList = try decoder.decode(BookList.self, from: bookList)
 
+        do {
+
+            let decodedBookList = try decoder.decode(BookList.self, from: bookList)
+ 
             return decodedBookList.items
         } catch {
             return nil
@@ -75,21 +76,24 @@ class BooksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
     func searchForBooks(_ searchTerm: String) {
+        print(searchTerm)
         if searchTerm.count == 0 {
             return
         }
         
-        if let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=\(searchTerm)&key=AIzaSyDn5fVmCB5yXnu27xzMckoY_r05bCmGe88") {
+        if let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=\(searchTerm)&key=AIzaSyDn5fVmCB5yXnu27xzMckoY_r05bCmGe88&maxResults=15") {
             let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
             let task = session.dataTask(with: url) { (data, response, error) in
-                // This will run when the network request returns
+
+                let dataString = String(data: data!, encoding: String.Encoding.utf8)! as String
+                print(dataString)
+//                 This will run when the network request returns
                 if let error = error {
                     print(error.localizedDescription)
                 } else if let data = data {
                     if let bookList = self.parseJSON(data) {
+    
                         self.books.bookList = bookList                }
-                  
-                    // print(self.books.bookList)
 
                     self.tableView.reloadData()
                 }
